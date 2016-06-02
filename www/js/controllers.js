8b66b2a20adfecf827b2994dff5edad9ae247ef1
Tom Coxen <tom@flashquartermaster.com>
2016-06-02 12:55:16 +0100

    Show banner ad on navigating to charity and navigating home

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,63 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, DeveloperSettings) {
+
+  $scope.developerSettings = DeveloperSettings.get();
+
+  $scope.$on('$ionicView.enter', function() {
+
+    console.log('<GFF> Platform: ', $scope.developerSettings.id);
+
+    AdMob.removeBanner();
+    AdMob.createBanner( {
+        adId: $scope.developerSettings.banner,
+        isTesting: true,
+        overlap: false,
+        offsetTopBar: false,
+        adSize:'SMART_BANNER',
+        position: AdMob.AD_POSITION.BOTTOM_CENTER,
+        overlap:false,
+        bgColor: 'black',
+        autoShow: true
+    } );
+
+    console.log('<GFF> Developer AdUnit: ', $scope.developerSettings.banner);
+
+    /*AdMob.prepareInterstitial({
+        adId: $scope.developerSettings.interstitial,
+        autoShow: true
+    });*/
+  })
+
+})
 
 .controller('GiveCtrl', function($scope, Charities) {
   $scope.charities = Charities.all();
 })
 
 .controller('GiveDetailCtrl', function($scope, $stateParams, Charities ){
+
   $scope.charity = Charities.get($stateParams.charityId);
+
+  $scope.$on('$ionicView.enter', function() {
+      AdMob.removeBanner();
+      AdMob.createBanner({
+         adId: $scope.charity.banner,
+         isTesting: true,
+         offsetTopBar: true,
+         adSize:'SMART_BANNER',
+         position: AdMob.AD_POSITION.BOTTOM_CENTER,
+         bgColor: 'black',
+         autoShow: true,
+         /*success: function(){
+           //alert('New banner');
+         },
+         error: function(){
+           //alert('failed to create banner');
+         }*/
+     });
+     console.log('<GFF> Charity AdUnit: ', $scope.charity.banner);
+  })
 })
 
 .controller('SettingsCtrl', function($scope) {});
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,36 @@
 angular.module('starter.services', [])
 
+.factory('DeveloperSettings', function(){
+
+  var platformSettings = [{
+    id: 'ios',
+    banner: 'ca-app-pub-9425381356824619/4451731633',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
+  }, {
+    id: 'android',
+    banner: 'ca-app-pub-9425381356824619/5928464836',
+    interstitial: 'ca-app-pub-9425381356824619/3510368838'
+  }, {
+    id: 'wp8',
+    banner: 'ca-app-pub-9425381356824619/7405198034',
+    interstitial: 'ca-app-pub-9425381356824619/6463835232'
+  }];
+
+  return {
+    get: function() {
+      if (/(ipod|iphone|ipad)/i.test(navigator.userAgent) ){
+        return platformSettings[0];
+      } else if( /(android)/i.test(navigator.userAgent) ) {
+        return platformSettings[1];
+      } else {
+        return platformSettings[2];
+      }
+      return null;
+    }
+
+  };
+})
+
 .factory('Charities', function() {
   // Some fake testing data
   var charities = [{
@@ -7,79 +38,106 @@ angular.module('starter.services', [])
     name: 'Charity 1',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    /*initial data for ios only*/
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 1,
     name: 'Charity 2',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 2,
     name: 'Charity 3',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 3,
     name: 'Charity 4',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 4,
     name: 'Charity 5',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 5,
     name: 'Charity 6',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 6,
     name: 'Charity 7',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 7,
     name: 'Charity 8',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 8,
     name: 'Charity 9',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 9,
     name: 'Charity 10',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 10,
     name: 'Charity 11',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 11,
     name: 'Charity 12',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }, {
     id: 12,
     name: 'Charity 13',
     thumbnail: 'img/breast_cancer.png',
     url: 'https://www.breastcancercare.org.uk/',
-    description: 'A charity description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel gravida sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In laoreet efficitur nunc euismod lacinia.',
+    banner: 'ca-app-pub-9425381356824619/6786736033',
+    interstitial: 'ca-app-pub-9425381356824619/8219770039'
   }];
 
   return {
