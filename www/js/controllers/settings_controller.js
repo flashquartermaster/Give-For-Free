e86c77209b4eb486c014eeee25e219d721552b9f
Tom Coxen <tom@flashquartermaster.com>
2016-06-10 09:32:35 +0100

    Bug: Can have no locations switched on

--- www/js/controllers/settings_controller.js
+++ www/js/controllers/settings_controller.js
@@ -1,15 +1,43 @@
-controllers.controller('SettingsCtrl', function($scope, Settings, $state, $ionicHistory) {
+controllers.controller('SettingsCtrl', function($scope, Settings, $state, $ionicHistory, $ionicPopup) {
   $scope.platformSettings = Settings.getPlatformSettings();
   $scope.adTypes = Settings.getAdTypes();
   $scope.locations = Settings.getLocations();
 
   $scope.onAdTypeChange = function( adType ){
-    //Radio function for checkboxes
-    var arrayLength = $scope.adTypes.length;
-    for (var i = 0; i < arrayLength; i++) {
+    //Radio function
+    var len = $scope.adTypes.length, i;
+    for (i = 0; i < len; i++) {
       if( $scope.adTypes[i].id != adType.id ){
         $scope.adTypes[i].isOn = false;
       }
+    }
+  }
+
+  $scope.onLocationChange = function( adId ){
+    var itemToggleStatus = [], i;
+    var len = $scope.locations.length;
+    for(i = 0; i < len; i++){
+      if ($scope.locations[i].isOn === false){
+        itemToggleStatus.push(i);
+      }
+    }
+
+    if(itemToggleStatus.length === len){
+      //All Off
+      var alertPopup = $ionicPopup.alert({
+         title: 'Locations',
+         template: 'If you switch off all the locations you will not be able to choose a charity to support'
+      });
+
+      alertPopup.then(function(response) {
+        var i;
+        var len = $scope.locations.length;
+        for(i = 0; i < len; i++){
+          if ($scope.locations[i].id === adId){
+            $scope.locations[i].isOn = true;
+          }
+        }
+      });
     }
   }
 
