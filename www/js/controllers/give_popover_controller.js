2cdaad96ab3e357cb8e72361831bc2ffdf9a8d9d
Tom Coxen <tom@flashquartermaster.com>
2016-06-20 11:13:08 +0100

    Comments on the bug fix just submitted

--- www/js/controllers/give_popover_controller.js
+++ www/js/controllers/give_popover_controller.js
@@ -7,7 +7,10 @@ controllers.controller('GivePopOverCtrl', function($scope, Scopes) {
 
   $scope.onPopoverReorderToggle = function(){
     //$scope.popover and $scope.isReordering are set in GiveCtrl so why the fuck I can get at the
-    //popover and not the other vaiables is a mystery to me
+    //popover and not the other vaiables is a mystery to me. I can pop the scope into the creation of the popover
+    //and the variables are readable but not updateable
+    //Also due to some mysterious prototyping issue in javascript the variables have to go inside an object
+    //data in this case
     if( Scopes.get('GiveCtrl').isSearch ){
       Scopes.get('GiveCtrl').isSearch = $scope.data.isSearch = false
     }
